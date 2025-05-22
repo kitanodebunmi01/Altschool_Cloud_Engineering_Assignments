3d8773251ec518bd94da7de0154383ba9374ba94
Ogooluwakitan Odebunmi <odebunmiogooluwakitan@gmail.com>
2025-05-22 22:54:32 +0100

    Created a script to count the number of files that exists in a specified directory

---
+++ Assignment_1/file_number.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+#count=0
+
+#for file in /Users/__kitan__/Desktop/Altschool-Africa/cloud_engineering/Altschool_Cloud_Engineering_Assignments/Assignment_1; do
+#    [ -f "$file" ] && ((count++))
+#done
+
+
+directory=/Users/__kitan__/Desktop/Altschool-Africa/cloud_engineering/Altschool_Cloud_Engineering_Assignments/Assignment_1
+count=$(find $directory -maxdepth 1 -type f | wc -l)
+
+read -p "What directory are we checking:"
+echo "Number of files in the specified directory: $count"
+
+
+
+
+
+
+
