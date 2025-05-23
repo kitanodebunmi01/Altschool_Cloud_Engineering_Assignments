992ccef7cdd70969eb3eef79af57108a5c0732dd
Ogooluwakitan Odebunmi <odebunmiogooluwakitan@gmail.com>
2025-05-23 19:57:36 +0100

    made a minor change to my file number script

--- Assignment_1/file_number.sh
+++ Assignment_1/file_number.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 
-#count=0
-
-#for file in /Users/__kitan__/Desktop/Altschool-Africa/cloud_engineering/Altschool_Cloud_Engineering_Assignments/Assignment_1; do
-#    [ -f "$file" ] && ((count++))
-#done
-
-
-directory=/Users/__kitan__/Desktop/Altschool-Africa/cloud_engineering/Altschool_Cloud_Engineering_Assignments/Assignment_1
+directory=DIR
 count=$(find $directory -type f | wc -l)
 
 read -p "What directory are we checking:"
